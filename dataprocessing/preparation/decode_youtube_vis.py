36111ac0eb4f84a150bc7d4bff7cf3a1a9f9fb30
Erling Ljunggren <erlingljunggren@gmail.com>
2021-03-14 12:13:12 +0100

    decoding rle and writing pngs with resulting mask

--- dataprocessing/preparation/decode_youtube_vis.py
+++ dataprocessing/preparation/decode_youtube_vis.py
@@ -1,9 +1,81 @@
+import os
 from utils import io_utils
 from pycocotools import mask as maskUtils
 import json
 import numpy as np
+import pandas as pd
+from collections import defaultdict
+from tqdm import tqdm
+import multiprocessing as mp
 
-if __name__ == "__main__":
+class YoutubeVISDecoder:
+    def __init__(self, file_root_dir, json_path=None):
+        self.file_root_dir = file_root_dir
+        
+        self.video_files = defaultdict(list) # video_id -> [frame_path, ...]
+        self.video_sizes = defaultdict(list) # video_id -> [height, width]
+        # video_id -> {timestep -> {category_id -> [rle, ...]
+        self.annotations = defaultdict(lambda : defaultdict(lambda : defaultdict(list)))  
+        self.segm_files = defaultdict(list)  # video_id -> [frame_path, ...]   
+
+        if json_path is not None:
+            self.index_data(json_path)
+
+    def index_data(self, json_path):
+        print("reading json file")
+        with open(json_path) as f:
+            data = json.load(f)
+
+        print("indexing videos")
+        for video_dict in data["videos"]:
+            self.video_files[video_dict["id"]] = video_dict["file_names"]
+            self.video_sizes[video_dict["id"]] = [video_dict["height"], video_dict["width"]]
+
+        print("indexing annotations")
+        for ann_dict in data["annotations"]:
+            for i, segm in enumerate(ann_dict["segmentations"]):
+                self.annotations[ann_dict["video_id"]][i][ann_dict["category_id"]].append(segm)
+
+    def decode_annotations(self, dest_dir):
+        if not os.path.isdir(dest_dir):
+            print("making dest dir", dest_dir)
+            os.makedirs(dest_dir)
+        for video_id in tqdm(self.video_files.keys(), desc="decoding annotations", unit="video"): #TODO make mp
+            h, w = self.video_sizes[video_id]
+
+            for i, segms in enumerate(self.annotations[video_id].values()):
+                mask = np.zeros([h, w], dtype=np.uint8)
+                for cat_id, segm in segms.items():
+                    for obj in segm: # aggregating here is what makes us blend instances
+                        if obj is None: continue
+                        if type(obj) == list:
+                            # polygon -- a single object might consist of multiple parts
+                            # we merge all parts into one mask rle code
+                            rles = maskUtils.frPyObjects(obj, h, w)
+                            rle = maskUtils.merge(rles)
+                        elif type(obj['counts']) == list:
+                            # uncompressed RLE
+                            rle = maskUtils.frPyObjects(obj, h, w)
+                        else:
+                            # rle
+                            rle = ann['segmentation']
+                        m = maskUtils.decode(rle)
+                        mask += m * cat_id
+                # png format is considerably smaller than numpy arrays 3kb < 900kb
+                mask_path = os.path.join(dest_dir, os.path.splitext(self.video_files[video_id][i])[0] + "_mask.png")
+                if not io_utils.write_img(mask, mask_path):
+                    print("could not write mask", mask_path)
+                self.segm_files[video_id].append(mask_path)
+
+    def write_samples_df(self, dest_path):
+        io_utils.make_folder_structure(dest_path)
+
+        
+
+
+
+
+def working_attempt():
     json_path = "data/train/instances.json"
     with open(json_path) as f:
         data = json.load(f)
@@ -30,4 +102,12 @@ if __name__ == "__main__":
 
     m = maskUtils.decode(rle)
     print(m.shape)
+
+    import pdb; pdb.set_trace()
+
+if __name__ == "__main__":
+    #working_attempt()
+    decoder = YoutubeVISDecoder("data/train/JPEGImages", "data/train/instances.json")
+    decoder.decode_annotations("data/train/semantic_segmentations")
+    decoder.write_samples_df("data/train/samples.csv")
 
--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,9 +1,23 @@
 import cv2
-import numpy
+import numpy as np
 import json
+import os
 
 def read_img(path):
     return cv2.imread(path)
+
+def write_img(img, path):
+    make_folder_structure(path)
+    return cv2.imwrite(path, img)
+
+def write_npy(arr, path):
+    make_folder_structure(path)
+    return np.save(path, arr)
+    
+def make_folder_structure(path):
+    dir_ = os.path.dirname(path)
+    if not os.path.isdir(dir_):
+        os.makedirs(dir_)
 
 
 if __name__ == "__main__":
