d16269a7388e1128579a5816413d20a1e5d4285c
Erling Ljunggren <erlingljunggren@gmail.com>
2021-03-14 13:59:12 +0100

    mp speeds

--- dataprocessing/preparation/decode_youtube_vis.py
+++ dataprocessing/preparation/decode_youtube_vis.py
@@ -8,6 +8,12 @@ from collections import defaultdict
 from tqdm import tqdm
 import multiprocessing as mp
 
+def ddict2dict(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            d[k] = ddict2dict(v)
+    return dict(d)
+
 class YoutubeVISDecoder:
     def __init__(self, file_root_dir, json_path=None):
         self.file_root_dir = file_root_dir
@@ -30,46 +36,64 @@ class YoutubeVISDecoder:
         for video_dict in data["videos"]:
             self.video_files[video_dict["id"]] = video_dict["file_names"]
             self.video_sizes[video_dict["id"]] = [video_dict["height"], video_dict["width"]]
+        
+        self.video_files = ddict2dict(self.video_files)
+        self.video_sizes = ddict2dict(self.video_sizes)
 
         print("indexing annotations")
         for ann_dict in data["annotations"]:
             for i, segm in enumerate(ann_dict["segmentations"]):
                 self.annotations[ann_dict["video_id"]][i][ann_dict["category_id"]].append(segm)
 
+        self.annotations = ddict2dict(self.annotations)
+
     def decode_annotations(self, dest_dir):
         if not os.path.isdir(dest_dir):
             print("making dest dir", dest_dir)
             os.makedirs(dest_dir)
-        for video_id in tqdm(self.video_files.keys(), desc="decoding annotations", unit="video"): #TODO make mp
-            h, w = self.video_sizes[video_id]
 
-            for i, segms in enumerate(self.annotations[video_id].values()):
-                mask = np.zeros([h, w], dtype=np.uint8)
-                for cat_id, segm in segms.items():
-                    for obj in segm: # aggregating here is what makes us blend instances
-                        if obj is None: continue
-                        if type(obj) == list:
-                            # polygon -- a single object might consist of multiple parts
-                            # we merge all parts into one mask rle code
-                            rles = maskUtils.frPyObjects(obj, h, w)
-                            rle = maskUtils.merge(rles)
-                        elif type(obj['counts']) == list:
-                            # uncompressed RLE
-                            rle = maskUtils.frPyObjects(obj, h, w)
-                        else:
-                            # rle
-                            rle = ann['segmentation']
-                        m = maskUtils.decode(rle)
-                        mask += m * cat_id
-                # png format is considerably smaller than numpy arrays 3kb < 900kb
-                mask_path = os.path.join(dest_dir, os.path.splitext(self.video_files[video_id][i])[0] + "_mask.png")
-                if not io_utils.write_img(mask, mask_path):
-                    print("could not write mask", mask_path)
-                self.segm_files[video_id].append(mask_path)
+
+        with mp.Pool(16) as pool:
+            self.segm_files = {video_id: vid_segm_files for video_id, vid_segm_files in zip(
+                (v for v in self.video_files.keys()), 
+                tqdm(pool.imap(worker, 
+                    ((self.annotations[video_id], self.video_sizes[video_id], self.video_files[video_id], dest_dir ) 
+                        for video_id in self.video_files.keys()
+                    )
+                ), desc="decoding annotations", unit="video", total=len(self.video_files)))}
 
     def write_samples_df(self, dest_path):
         io_utils.make_folder_structure(dest_path)
 
+def worker(inputs):
+    annotations, size, video_paths, dest_dir = inputs
+    h, w = size
+    segm_files = []
+
+    for i, segms in enumerate(annotations.values()):
+        mask = np.zeros([h, w], dtype=np.uint8)
+        for cat_id, segm in segms.items():
+            for obj in segm: # aggregating here is what makes us blend instances
+                if obj is None: continue
+                if type(obj) == list:
+                    # polygon -- a single object might consist of multiple parts
+                    # we merge all parts into one mask rle code
+                    rles = maskUtils.frPyObjects(obj, h, w)
+                    rle = maskUtils.merge(rles)
+                elif type(obj['counts']) == list:
+                    # uncompressed RLE
+                    rle = maskUtils.frPyObjects(obj, h, w)
+                else:
+                    # rle
+                    rle = ann['segmentation']
+                m = maskUtils.decode(rle)
+                mask += m * cat_id
+        # png format is considerably smaller than numpy arrays 3kb < 900kb
+        mask_path = os.path.join(dest_dir, os.path.splitext(video_paths[i])[0] + "_mask.png")
+        if not io_utils.write_img(mask, mask_path):
+            print("could not write mask", mask_path)
+        segm_files.append(mask_path)
+    return segm_files
         
 
 
