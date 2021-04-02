bc837fd790d118fea51ba9de51ac5f8e650746c6
Erling Ljunggren <erlingljunggren@gmail.com>
2021-04-02 18:04:37 +0200

    I remembered pycocotools is a pip package

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="video-segmentation",
     packages=["dataprocessing", "evaluation", "models", "training", "utils"],
     install_requires=[
+        "wheel",
         "pandas>=1.2",
         "numpy>=1.20",
         "tqdm",
@@ -11,6 +12,7 @@ setup(
         "dill>=0.3",
         "matplotlib>=3.3",
         "opencv-python>=4.5",
+        "pycocotools>=2",
         "multiprocess>=0.70"
     ]
 )
