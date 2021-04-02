1a2d4d0f21dc2267adb9a42f354a83acbaa48a32
Erling Ljunggren <erlingljunggren@gmail.com>
2021-04-02 17:55:53 +0200

    direnv setup

--- setup.py
+++ setup.py
@@ -3,5 +3,15 @@ from distutils.core import setup
 setup(
     name="video-segmentation",
     packages=["dataprocessing", "evaluation", "models", "training", "utils"],
+    install_requires=[
+        "pandas>=1.2",
+        "numpy>=1.20",
+        "tqdm",
+        "torch>=1.7",
+        "dill>=0.3",
+        "matplotlib>=3.3",
+        "opencv-python>=4.5",
+        "multiprocess>=0.70"
+    ]
 )
 
