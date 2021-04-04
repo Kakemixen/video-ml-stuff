a56d7b2019f3a3457b0a4dade2b828a50a4dfdcf
Erling Ljunggren <erlingljunggren@gmail.com>
2021-04-04 21:42:40 +0200

    make segmentor encapsulate encoder backbone

--- models/segmentors/basic_segmentors.py
+++ models/segmentors/basic_segmentors.py
@@ -2,19 +2,24 @@ import torch
 from torch import nn as nn
 
 from models.blocks.conv_blocks import ConvBNReLU
+from models.encoders.basic_encoders import TrivialEncoder
 
 class TrivialSegmentor(nn.Module):
-    def __init__(self, in_c=32, dec_c=16, out_c=1, upscale_x=1):
+    def __init__(self, in_c=3, enc_c=16, latent_c=64, out_c=1, downscale_x=1):
         super().__init__()
-        blocks = [ ConvBNReLU(in_c, dec_c),
+        self.encoder = TrivialEncoder(in_c=in_c, enc_c=enc_c, out_c=latent_c, downscale_x=downscale_x)
+
+        blocks = [ ConvBNReLU(latent_c, enc_c),
                    nn.UpsamplingBilinear2d(scale_factor=2) ]
-        for _ in range(1, upscale_x):
-            blocks.append(ConvBNReLU(dec_c, dec_c))
+        for _ in range(1, downscale_x):
+            blocks.append(ConvBNReLU(enc_c, enc_c))
             blocks.append(nn.UpsamplingBilinear2d(scale_factor=2))
-        blocks.append(ConvBNReLU(dec_c, out_c, ks=1, s=1, p=0, 
+        blocks.append(ConvBNReLU(enc_c, out_c, ks=1, s=1, p=0, 
             batch_norm=False, activation=False))
-        self.blocks = nn.Sequential(*blocks)
+        self.decoder = nn.Sequential(*blocks)
 
     def forward(self, x):
-        return self.blocks(x)
+        feat = self.encoder(x)
+        feat = self.decoder(feat)
+        return feat
 
--- tests/models/test_model_segmentors.py
+++ tests/models/test_model_segmentors.py
@@ -11,7 +11,7 @@ class TestModelSegmentors:
     @classmethod
     def setup_class(cls):
         cls.batch_size = 5
-        cls.in_c = 64
+        cls.in_c = 3
         cls.out_c = 20
         cls.h = 4
         cls.w = 8
@@ -24,12 +24,12 @@ class TestModelSegmentors:
                         )
 
     def test_trivial_encoder_out_shape_down_2(self):
-        encoder = TrivialSegmentor(in_c=self.in_c, out_c=self.out_c, upscale_x=2)
+        encoder = TrivialSegmentor(in_c=self.in_c, out_c=self.out_c, downscale_x=2)
         
         out = encoder(self.rand_input)
         assert torch.is_tensor(out)
         assert out.dim() == 4
         assert out.shape[1] == self.out_c
-        assert out.shape[2] == self.h * 2**2
-        assert out.shape[3] == self.w * 2**2
+        assert out.shape[2] == self.h
+        assert out.shape[3] == self.w
 
