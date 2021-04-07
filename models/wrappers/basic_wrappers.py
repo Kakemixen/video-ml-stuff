f1f4e5c0b6dee092f796e84070ed8f5b9f7e29ae
Erling Ljunggren <erlingljunggren@gmail.com>
2021-04-07 22:29:59 +0200

    video generator wrapper

--- models/wrappers/basic_wrappers.py
+++ models/wrappers/basic_wrappers.py
@@ -16,4 +16,31 @@ class TrainingWrapper(nn.Module):
         loss = loss_func(pred, target)
         if propagate:
             loss.backward()
+        return loss.detach()
+
+class VideoGeneratorWrapper(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x):
+        for frame in x:
+            yield self.model(frame)
+
+    def predict(self, x):
+        for frame in self(x):
+            yield frame["prediction"]
+
+    def calculate_loss(self, inputs, targets, loss_func, propagate=True):
+        # transpose input and target
+        inputs = inputs.permute(1,0,2,3,4)
+        targets = targets.permute(1,0,2,3,4)
+
+        pred_gen = self(inputs)
+        loss = 0
+        for pred, target in zip(pred_gen, targets):
+            batch_loss = loss_func(pred, target)
+            if propagate:
+                batch_loss.backward()
+            loss += batch_loss.detach()
         return loss 
--- tests/models/test_model_wrappers.py
+++ tests/models/test_model_wrappers.py
@@ -1,54 +1,97 @@
 import pytest
 import torch
 
+from collections.abc import Iterable
+
 from tests.helpers import dummy_data
 from unittest.mock import MagicMock
     
 from models.wrappers.basic_wrappers import TrainingWrapper, VideoGeneratorWrapper
 
-class TestModelWrappers:
+class TestTrainingWrapper:
 
     @classmethod
     def setup_class(cls):
         cls.rand_input, cls.rand_pred, cls.corr_pred, cls.target = \
                 dummy_data.create_dummy_batch(batch_size=5)
+        identity = lambda x: {"prediction": x}
+        cls.wrapped = TrainingWrapper(identity)
 
     def test_training_wrapper_forward_pass_dict(self):
-        identity = lambda x: {"prediction": x}
-        wrapped = TrainingWrapper(identity)
 
-        forward_out = wrapped(self.rand_pred)
+        forward_out = self.wrapped(self.rand_pred)
         assert isinstance(forward_out, dict)
         assert (forward_out["prediction"] == self.rand_pred).all()
         
     def test_training_wrapper_different_predict_in_different_out(self):
-        identity = lambda x: {"prediction": x}
-        wrapped = TrainingWrapper(identity)
-        assert (wrapped.predict(self.rand_pred) == self.rand_pred).all()
+        assert (self.wrapped.predict(self.rand_pred) == self.rand_pred).all()
 
-        assert (wrapped.predict(self.rand_input) != self.rand_pred).all()
+        assert (self.wrapped.predict(self.rand_input) != self.rand_pred).all()
 
     def test_training_wrapper_loss_backward_is_called(self):
-        identity = lambda x: {"prediction": x}
-        wrapped = TrainingWrapper(identity)
-
         loss_return = torch.Tensor(0)
-        loss_return.backward = MagicMock()
-        loss_return.backward._mock_name = "backward pass"
+        loss_return.backward = MagicMock(name="backward_pass")
+        loss_return.detach = MagicMock(name="detach_call")
         loss = lambda p,t: loss_return
 
-        loss_out = wrapped.calculate_loss(self.rand_pred, self.target, loss)
+        loss_out = self.wrapped.calculate_loss(self.rand_pred, self.target, loss)
         loss_return.backward.assert_called()
+        loss_return.detach.is_called()
 
     def test_training_wrapper_loss_backward_is_suppressed(self):
-        identity = lambda x: {"prediction": x}
-        wrapped = TrainingWrapper(identity)
-
         loss_return = torch.Tensor(0)
-        loss_return.backward = MagicMock()
-        loss_return.backward._mock_name = "backward pass"
+        loss_return.backward = MagicMock(name="backward_pass")
+        loss_return.detach = MagicMock(name="detach_call")
         loss = lambda p,t: loss_return
 
-        loss_out = wrapped.calculate_loss(self.rand_pred, self.target, 
+        loss_out = self.wrapped.calculate_loss(self.rand_pred, self.target, 
                 loss, propagate=False)
         loss_return.backward.not_called()
+        loss_return.detach.is_called()
+
+class TestVideoGenWrapper:
+
+    @classmethod
+    def setup_class(cls):
+        cls.rand_input, cls.rand_pred, cls.corr_pred, cls.target = \
+                dummy_data.create_dummy_batch(batch_size=5)
+        cls.rand_input = torch.stack([cls.rand_input for _ in range(3)])
+        cls.rand_pred  = torch.stack([cls.rand_pred for _ in range(3)])
+        cls.corr_pred  = torch.stack([cls.corr_pred for _ in range(3)])
+        cls.target     = torch.stack([cls.target for _ in range(3)])
+        identity = lambda x: {"prediction": x}
+        cls.wrapped = VideoGeneratorWrapper(identity)
+
+    def test_training_wrapper_forward_pass_dict(self):
+
+        forward_out = self.wrapped(self.rand_pred)
+        assert isinstance(forward_out, Iterable)
+        assert (next(forward_out)["prediction"] == self.rand_pred).all()
+        
+    def test_training_wrapper_different_predict_in_different_out(self):
+        assert (next(self.wrapped.predict(self.rand_pred)) == \
+                self.rand_pred).all()
+
+        assert (next(self.wrapped.predict(self.rand_input)) != \
+                self.rand_pred).all()
+
+    def test_training_wrapper_loss_backward_is_called(self):
+        loss_return = torch.Tensor(0)
+        loss_return.backward = MagicMock(name="backward_pass")
+        loss_return.detach = MagicMock(name="detach_call")
+        loss = lambda p,t: loss_return
+
+        loss_out = self.wrapped.calculate_loss(self.rand_pred, self.target, loss)
+        loss_return.backward.assert_called()
+        loss_return.detach.is_called()
+
+    def test_training_wrapper_loss_backward_is_suppressed(self):
+        loss_return = torch.Tensor(0)
+        loss_return.backward = MagicMock(name="backward_pass")
+        loss_return.detach = MagicMock(name="detach_call")
+        loss = lambda p,t: loss_return
+
+        loss_out = self.wrapped.calculate_loss(self.rand_pred, self.target, 
+                loss, propagate=False)
+        loss_return.backward.not_called()
+        loss_return.detach.is_called()
